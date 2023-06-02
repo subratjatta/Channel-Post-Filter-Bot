9485fed36818012f2006dfe8682907d0f11957d1
SK <73738726+subratjatta@users.noreply.github.com>
2023-06-02 08:28:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,5 +3,5 @@ API_HASH     = "12d26192818156bfcbd9bd70d661657f"
 BOT_TOKEN    = "6244733203:AAH-VFoqQvMaYkdD9IxIlmS6iTFxbdT7VMo"
 SESSION      = "BQCcWgMU7pPCptrtr4bGeHA4Jv0d1bRrqajFHTtqBoKL_0X3SiKDdqJuOgRmJm9gLOn4DwkEsgDwCaA6GNILVfxntLJdeSQEZlQkc9FizOxgFAxjNR_tQRuAx8q6GUO0U81z4CF4VMjgrwnIOUKUgmX5QPzcrr_fNGIVjOUuqHwbsotIBtnXdqCx7aIu2ZHCDwRAjBUuSpSw4C0LqaQzYYIiWSBPwAK_-3qE3GyflGzPrZwpIuRGPmFFLO1w2TB7A30_zjPbYqv5Q3uxGexodwe6ZinIdSx6Vhj5VoWcDd5CuerfuBoGvDj45i3zv2EzBdBmA4soiTTIJyVqZp_AneCkFDxmiAA"
 DATABASE_URI = "mongodb+srv://sktech2022:munjanini143@cluster0.gqewzsl.mongodb.net/?retryWrites=true&w=majority"
-LOG_CHANNEL  = -1001980857896
+LOG_CHANNEL  = -1001919479067
 ADMIN        = 6051691339
